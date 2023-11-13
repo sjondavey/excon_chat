0552d5ef59379d491a00d301defbe572693e2b42
Steven <daveysj@gmail.com>
2023-11-13 10:17:41 +0200

    Empty prompts - fix

--- question_answering.py
+++ question_answering.py
@@ -120,7 +120,7 @@ if authentication_status:
 
 
     # User-provided prompt
-    if prompt := st.chat_input(disabled=not openai_api): 
+    if prompt := st.chat_input(): 
         if prompt and prompt != "":
             st.session_state.messages.append({"role": "user", "content": prompt})
             with st.chat_message("user"):
